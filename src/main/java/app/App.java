c3de137de63044c624c8ada8cbbe6683687d0ea8
rexstatic <paw94@live.dk>
2020-07-25 23:56:56 +0200

    Setting up Frame for GUI

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,13 +1,16 @@
 package app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+import javax.swing.JFrame;
+
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        JFrame obj1 = new JFrame();
+        obj1.setBounds(600, 250, 700, 600);
+        obj1.setResizable(false);
+        obj1.setVisible(true);
+        obj1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 }
